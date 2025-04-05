2c68c8e8885b2692d17048e6383c2a9a849edae3
aaronshin43 <aaronshin.exe@gmail.com>
2025-04-05 14:44:11 -0400

    Update route to login and register

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///exit.db'
@@ -12,7 +13,21 @@ class Item(db.Model):
     description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Float, nullable=False)
     category = db.Column(db.String(20), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     image_url = db.Column(db.String(200), default='/static/images/placeholder.jpg')
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password_hash = db.Column(db.String(200))
+    #items = db.relationship('Item', backref='seller', lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 @app.route('/')
 def home():
@@ -35,18 +50,7 @@ def search():
 
 @app.route('/marketplace', methods=['GET', 'POST'])
 def marketplace():
-    if request.method == 'POST':
-        # Handle form submission
-        name = request.form['name']
-        description = request.form['description']
-        price = request.form['price']
-        category = request.form['category']
-        image_url = request.form['image_url']
-        new_item = Item(name=name, description=description, price=price, category=category, image_url=image_url)
-        db.session.add(new_item)
-        db.session.commit()
-        return redirect('/marketplace')
-    elif request.method == 'GET':
+    if request.method == 'GET':
         # Get filter parameters from request
         categories = request.args.getlist('categories')
         min_price = request.args.get('min_price', type=float)
@@ -83,6 +87,14 @@ def marketplace():
     else:
         items = Item.query.all()
         return render_template('marketplace.html', items=items)
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
